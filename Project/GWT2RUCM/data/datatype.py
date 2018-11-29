ca3b6ceb724fcb11eb67660499bfa7be5fd3ccce
Patrick <dawnerz@163.com>
2018-11-29 18:52:35 +0800

    ..

--- Project/GWT2RUCM/data/datatype.py
+++ Project/GWT2RUCM/data/datatype.py
@@ -4,6 +4,10 @@ class GWT(object):
 
 class TagedGWT(GWT):
     __slots__ = ('useCaseName', 'BranchScenarios', 'preScenarios', 'postScenarios', 'flowType')
+class RUCM():
+    __slots__=('useCaseName','briefDescription','precondition','primaryActor','secondaryActors','dependency','generalization','basic','specific','bounded','global')
+    def __init__(self,name):
+        self.useCaseName=name
 
 
 class Sentence(object):
@@ -21,4 +25,32 @@ class Association(object):
 class Scenario(object):
     __slots__ = ('gwtId', 'conditionIds')
 
-
+class BasicFlow():
+    def __init__(self):
+        self.actions=[]
+        self.postCondition=''
+    def addAction(action):
+        self.actions.append(action)
+class SpecificFlow():
+    def __init__(self):
+        self.rfs=0
+        self.actions=[] 
+        self.postCondition=''
+    def addAction(action):
+        self.actions.append(action)
+class BoundedFlow():
+    def __init__(self):
+        self.rfs=[]
+        self.actions=[]
+        self.postCondition='' 
+    def addRFS(num):
+        self.rfs.append(num)
+    def addAction(action):
+        self.actions.append(action)
+class GlobalFlow():
+    def __init__(self):
+        self.condition=[]
+        self.actions=[] 
+        self.postCondition=''
+    def addAction(action):
+        self.actions.append(action)
--- Project/GWT2RUCM/response/rucmresponse.py
+++ Project/GWT2RUCM/response/rucmresponse.py
@@ -4,4 +4,9 @@ class RUCMGnerator():
     def __init__(self):
         self.nlp=NLPExecutor()
         self.lable=LableGenerator(self.nlp)
-        //TODO
+    def generateRUCMs(self):
+        '''
+        根据id获取要处理的GWT
+        为这一组GWT添加标签获得一组TaggedGWT
+        将这一组TaggedGWT合成一组RUCM
+        '''
